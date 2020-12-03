5f4088962076d24747cd6eb7467386aa004fa885
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-12-03 08:51:32 +0700

    first commit

---
+++ Assets/Script/mainmenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class mainmenu : MonoBehaviour
+{
+    public string arah;
+
+    public void GantiTujuan()
+    {
+    Application.LoadLevel(arah);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
