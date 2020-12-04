d7c5f362ef7b23bd12c4a3bb04bc290d2fdf9ad3
Muhammad Fadhli Hisyam <38355018+fadhlifadra@users.noreply.github.com>
2020-12-04 07:49:32 +0700

    percobaan selanjutnya

---
+++ Assets/Script/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement, speed, maxheight, minHeight;
+    public int health = 3;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxheight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
+    }
+}
---
+++ Assets/Script/spawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject[] obstacle;
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn, decreaseTime;
+    public float minTime = 0.65f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwSpawn <= 0)
+        {
+            int random = Random.Range(0, obstacle.Length);
+            Instantiate(obstacle[random], transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+            if (startTimeBtwSpawn > minTime)
+            {
+                startTimeBtwSpawn -= decreaseTime;
+            }
+        }
+        else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
